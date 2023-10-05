e0a802344d6ba0a6337e3894ade03bdb817e25be
listyantidewi1 <listyantidewi@gmail.com>
2023-10-05 09:34:09 +0700

    studikasus3

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template
+# import SQL untuk menggunakan bahasa SQL dalam python
+from cs50 import SQL
+# import tools utk website
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
 
 app = Flask(__name__)
 
-@app.route('/') #root route
-def hello_world(): #function hello_world
-    students = [
-        {"name": "Sandrine",  "score": 100},
-        {"name": "Gergeley", "score": 87},
-        {"name": "Frieda", "score": 92},
-        {"name": "Fritz", "score": 40},
-        {"name": "Sirius", "score": 75},
-    ]
-    return render_template('index.html', students=students)
+db = SQL("sqlite:///score.db")
+
+@app.route('/', methods=["GET", "POST"]) #root route
+def index(): #function index
+    if request.method == "POST":
+        name = request.form.get("name")
+        score = request.form.get("score")
+        db.execute("INSERT INTO score (name, score) values(?,?)", name, score)
+        return redirect("/")
+    else:
+        students = db.execute("select * from score")
+        return render_template("index.html", students=students)
